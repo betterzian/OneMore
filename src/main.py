b3fceb38dd4c3f87cdfbb31614041b67188b086f
betterzian <code_yuan@outlook.com>
2024-03-01 19:01:31 +0800

    update

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import torch
 if __name__ == '__main__':
     torch.multiprocessing.set_start_method('spawn', force=True)
     parser = argparse.ArgumentParser(description='param')
-    parser.add_argument('--test', action='store_false')
+    parser.add_argument('--test', action='store_true')
     parser.add_argument('--gather', action='store_false')
     parser.add_argument('--tl', type=int, default=17280)
     parser.add_argument('--tif', type=int, default=0)
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument('--tap', type=int, default=90)
     parser.add_argument('--ontn', type=int, default=5000)
     parser.add_argument('--oftn', type=int, default=700)
-    parser.add_argument('--filename', type=str, default="openb_pod_list_gpushare100")
+    parser.add_argument('--filename', type=str, default="openb_pod_list_multigpu50")
     parser.add_argument('--nt', type=list, default=((64, 2),(64, 8),(96, 4),(96, 8),(104, 2),(128, 1),(128, 8)))
     parser.add_argument('--nn', type=tuple, default=(10,10,10,10,10,10,10))
     parser.add_argument('--csv_name', type=str, default=None)
